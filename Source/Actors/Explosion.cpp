e62f4fea7f1ca5a7cdfe11616bb4beb79f222def
Victorats <110611893+Victorats@users.noreply.github.com>
2023-12-06 13:30:50 -0300

    uma leve ideia da explosao da bomba (imeplementada numa funcao nada haver)

--- Source/Actors/Explosion.cpp
+++ Source/Actors/Explosion.cpp
@@ -26,5 +26,24 @@ void Explosion::OnUpdate(float deltaTime)
 
 void Explosion::RenderExplosion()
 {
-    // Implemente a lógica para renderizar a explosão em forma de cruz aqui
+    mColliders = std::vector<ColliderComponent*>(9);
+    mDrawComponents = std::vector<DrawAnimatedComponent*>(9);
+    for (int i = 0; i < 9; i++) {
+        Vector2 colliderPosition = position;
+        if (i != 0) {
+            int direction = i % 4;
+            int distance = 32 * (i / 4 + 1);
+            switch (direction) {
+                case 0: colliderPosition.y -= distance; break; // Up
+                case 1: colliderPosition.x += distance; break; // Right
+                case 2: colliderPosition.y += distance; break; // Down
+                case 3: colliderPosition.x -= distance; break; // Left
+            }
+        }
+        mColliders[i] = new ColliderComponent(this, colliderPosition);
+        mDrawComponents[i] = new DrawAnimatedComponent(this,"../Assets/Sprites/Explosion/explosionsprite.png","../Assets/Sprites/Explosion/explosionsprite.json");
+        mDrawComponents[i]->AddAnimation("explode", {0,1,2,3,4,5});
+        mDrawComponents[i]->SetAnimation("explode");
+        mDrawComponents[i]->SetAnimFPS(10.0f);
+    }
 }
